d1b488782a470a2632d4a964e5db9347e144e50a
ilona <isaevaangelina@gmail.com>
2021-02-21 22:00:01 +0200

    Initial commit

---
+++ happy.js
@@ -0,0 +1,15 @@
+function womensAge(n) {
+    if ( n%2===0){
+        let x= Math.floor(n/2);
+
+        return (`${n}? That's just 20, in base ${x}!`);
+
+    }
+    else {
+        let x= Math.floor(n/2);
+        return  (`${n}? That's just 21, in base ${x}!`)  ;
+
+
+    }
+}
+//https://www.codewars.com/kata/5e96332d18ac870032eb735f
